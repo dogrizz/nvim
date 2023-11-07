cc3671adb3ed1ca92ba0410969d8ede17ae3d348
Dominik Zmitrowicz <dogrizz@gmail.com>
2023-11-07 13:19:42 +0100

    rename and move some stuff

---
+++ lua/core/init.lua
@@ -0,0 +1,4 @@
+require("core.remap")
+require("core.lazy")
+vim.g.did_load_filetypes = 1
+require("core.set")
---
+++ lua/core/lazy.lua
@@ -0,0 +1,37 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup(
+  { 
+    {
+	'nvim-telescope/telescope.nvim', tag = '0.1.4',
+	dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {
+	'nvim-treesitter/nvim-treesitter',
+	build = ":TSUpdate"
+    },
+    { 'rose-pine/neovim', name = 'rose-pine' },
+    { 'nathom/filetype.nvim' },
+    { 'mbbill/undotree' },
+    { 'tpope/vim-fugitive' },
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {'L3MON4D3/LuaSnip'},
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+  }
+, opts)
+
