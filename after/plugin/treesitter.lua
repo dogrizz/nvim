69de3f2abbbaf38f1152a1c83fad9b373ec5b22b
Dominik Zmitrowicz <dogrizz@gmail.com>
2023-11-07 22:30:58 +0100

    yet another scheme and formatting lua works

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,23 +1,22 @@
-require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "query", "kotlin", "java", "python", "markdown", "yaml", "make", "javascript", "xml" },
+require 'nvim-treesitter.configs'.setup {
+    -- A list of parser names, or "all" (the five listed parsers should always be installed)
+    ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "query", "kotlin", "java", "python", "markdown", "yaml", "make", "javascript", "xml" },
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
 
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = true,
 
-  -- List of parsers to ignore installing (or "all")
-  ignore_install = { },
+    -- List of parsers to ignore installing (or "all")
+    ignore_install = {},
 
-  highlight = {
-    enable = true,
+    highlight = {
+        enable = true,
 
-    additional_vim_regex_highlighting = false,
-  },
-  
-  vim.treesitter.language.register('cpp', 'keymap')
+        additional_vim_regex_highlighting = false,
+    },
+
+    vim.treesitter.language.register('cpp', 'keymap')
 }
-
