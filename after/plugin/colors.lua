ab69a795050f112e16ca4e94d5d57423c12d5657
Dominik Zmitrowicz <dogrizz@gmail.com>
2023-11-07 22:14:56 +0100

    different colorscheme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,14 +1,1 @@
-require('rose-pine').setup({
-    disable_background = true
-})
-
-function ColorStuff(color) 
-	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
-
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
-end
-
-ColorStuff()
+vim.cmd("colorscheme kanagawa-dragon")
--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -11,17 +11,16 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup(
-  { 
+require("lazy").setup({ 
     {
-	'nvim-telescope/telescope.nvim', tag = '0.1.4',
-	dependencies = { 'nvim-lua/plenary.nvim' }
+        'nvim-telescope/telescope.nvim', tag = '0.1.4',
+        dependencies = { 'nvim-lua/plenary.nvim' }
     },
     {
-	'nvim-treesitter/nvim-treesitter',
-	build = ":TSUpdate"
+        'nvim-treesitter/nvim-treesitter',
+        build = ":TSUpdate"
     },
-    { 'rose-pine/neovim', name = 'rose-pine' },
+    { 'rebelot/kanagawa.nvim' },
     { 'nathom/filetype.nvim' },
     { 'mbbill/undotree' },
     { 'tpope/vim-fugitive' },
@@ -32,6 +31,6 @@ require("lazy").setup(
     {'L3MON4D3/LuaSnip'},
     {'williamboman/mason.nvim'},
     {'williamboman/mason-lspconfig.nvim'},
-  }
+}
 , opts)
 
