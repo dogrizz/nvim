9ee5f0f547c0ea5bfbd80e4fce9525e87d576f29
Dominik Zmitrowicz <dogrizz@gmail.com>
2024-04-01 12:56:16 +0200

    it seems I had an error in filetype mapping

--- after/plugin/filetype.lua
+++ after/plugin/filetype.lua
@@ -3,9 +3,7 @@ vim.g.did_load_filetypes = 1
 require("filetype").setup({
     overrides = {
         extensions = {
-            keymap = "cpp"
-        },
-        extensions = {
+            keymap = "cpp",
             xml = "xml"
         }
     }
